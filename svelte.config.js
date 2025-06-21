c30e51ab0b2b4fc9d9af4c87fba3a7d8f09db9f3
Orion Z43 <kovalevd418@gmail.com>
2025-06-21 03:50:30 +0300

    Fix deploy.yml

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
-
     kit: {
         adapter: adapter({
             pages: 'build',
@@ -14,7 +12,7 @@ const config = {
             strict: true
         }),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/OrionZ43_bio' : ''
+            base: process.env.BASE_PATH || ''
         }
     }
 };
